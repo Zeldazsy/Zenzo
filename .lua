dd87c501087a037546625d866a6093ada16af34f
Zelda <166204162+Zeldazsy@users.noreply.github.com>
2025-04-22 17:21:15 +0700

    Update .lua

--- .lua
+++ .lua
@@ -1,9 +1,13 @@
-if ZZ_LOADED then
-    error("\xe0\xb8\xa3\xe0\xb8\xb1\xe0\xb8\x99\xe0\xb9\x84\xe0\xb8\xa1\xe0\xb8\xab\xe0\xb8\xa5\xe0\xb8\xb2\xe0\xb8\xa2\xe0\xb8\x84\xe0\xb8\xa3\xe0\xb8\xb1\xe0\xb8\x87", 0)
-   return
-end
-pcall(function() getgenv().ZZ_LOADED = true end)
+repeat wait() until game:IsLoaded()
 
-local gameId = game.GameId
-if gameId ~= 6945584306 then return end -- ตอนนี้มีเกมเดียวเกมอื่นขี้เกียจทำ
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeldazsy/Zenzo/refs/heads/game/"..gameId))()
+    if ZZ_LOADED then
+        return
+    end
+    pcall(function() getgenv().ZZ_LOADED = true end)
+
+    if not _G.Authorize or _G.Authorize == "" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeldazsy/Zenzo/refs/heads/free" .. game.GameId))()
+        return
+    end
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeldazsy/Zenzo/refs/heads/paid" .. game.GameId .. "-wl"))()
